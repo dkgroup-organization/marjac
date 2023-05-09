af49dd81addce7f283a4a5c88518f7b91a063922
Mehdi HJ <itsxenon@outlook.fr>
2023-05-09 10:40:56 +0200

    d

--- marjac_extra/__manifest__.py
+++ marjac_extra/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Ajout champs divers dans les livraisons et leurs rapports",
     "version": "1.0",
-    "depends": ['base', 'sale', 'account', 'stock'],
+    "depends": ['base', 'sale', 'account', 'stock', 'product_expiry'],
     "author": "Mehdi Hajji",
     "summary": "Date de fabrication, Date d'expiration, Nbre d'unité etc...",
     'website': 'http://www.dkdigital.fr',
@@ -12,6 +12,7 @@
     "init_xml": [],
     'data': [
         'views/report_delivery_document.xml',
+        'views/stock_lot_view.xml',
     ],
     'demo_xml': [],
     'installable': True,
